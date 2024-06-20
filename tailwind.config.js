b93a13789c0cced3c9576f6e2828bed69e381aa0
Fady Yeta <FadyYeta@gmail.com>
2024-06-20 18:08:59 +0200

    refresh tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	darkMode: "selector",
-	content: [
-		"./app/**/*.{js,ts,jsx,tsx,mdx}",
-		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./components/**/*.{js,ts,jsx,tsx,mdx}",
-
-		// Or if using `src` directory:
-		"./src/**/*.{js,ts,jsx,tsx,mdx}",
-	],
+	content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
 	theme: {
 		extend: {},
 	},
